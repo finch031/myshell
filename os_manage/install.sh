bb23b80d6558ce707a264edef04357dbe9a9ca87
yusheng <64625784+finch031@users.noreply.github.com>
2022-09-13 10:01:22 +0800

    Create install.sh

---
+++ os_manage/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# nohup wget https://archive.apache.org/dist/hadoop/common/stable/hadoop-3.3.1.tar.gz >> install.log 2>&1 &
+
+# nohup wget https://dlcdn.apache.org/hbase/stable/hbase-2.3.6-bin.tar.gz >> install.log 2>&1 &
+
+# nohup wget https://dlcdn.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz >> install.log 2>&1 &
+
+# nohup wget https://dlcdn.apache.org/kylin/apache-kylin-3.1.2/apache-kylin-3.1.2-bin-hadoop3.tar.gz >> install.log 2>&1 &
+
+# nohup wget https://archive.apache.org/dist/kylin/apache-kylin-2.6.6/apache-kylin-2.6.6-bin-hadoop3.tar.gz >> install.log 2>&1 &
+
+nohup wget https://archive.apache.org/dist/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz >> install.log 2>&1 &
